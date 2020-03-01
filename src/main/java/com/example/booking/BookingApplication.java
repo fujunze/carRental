41ca299b54aa014e55a7ced1049dd6ee716017a5
LJay Fu <ljayfu@LJays-MacBook-Pro.local>
2020-03-01 15:22:05 +0800

    simple car rental

--- src/main/java/com/example/booking/BookingApplication.java
+++ src/main/java/com/example/booking/BookingApplication.java
@@ -1,11 +1,9 @@
 package com.example.booking;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan("microservice.qssj.mapper")
 public class BookingApplication {
 
 	public static void main(String[] args) {
