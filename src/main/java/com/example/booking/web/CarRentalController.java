957fc711e014ccbea270dd2ee9891951912d3b0a
LJay Fu <ljayfu@LJays-MacBook-Pro.local>
2020-03-01 15:26:38 +0800

    update

--- src/main/java/com/example/booking/web/CarRentalController.java
+++ src/main/java/com/example/booking/web/CarRentalController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.math.BigDecimal;
 
@@ -14,13 +13,6 @@ public class CarRentalController {
 
     @Autowired
     private CarRentalService carRentalService;
-
-    @RequestMapping("/car/index")
-    public ModelAndView index() {
-        ModelAndView view = new ModelAndView();
-        view.setViewName("index");
-        return view;
-    }
 
     /**
      * 租车
